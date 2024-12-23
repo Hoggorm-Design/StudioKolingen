db6d2870ba1e76a5244c95175130b277825b23b6
eirikek <eirik.engen.kvam@gmail.com>
2024-12-23 11:54:13 +0100

    Initialize Sanity Studio setup and create pages.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home: React.FC = () => {
-    return <h1 className="text-3xl font-bold">Velkommen til Hjemmesiden!</h1>;
+    return <h1 className="text-3xl font-bold">Welcome to the homepage!</h1>;
 };
 
 export default Home;
