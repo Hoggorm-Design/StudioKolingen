d056df0aa550909920bd6646baf12a3088e11814
August Solli Middelkoop <august.kampen@gmail.com>
2025-01-25 14:56:06 +0100

    Enhancement: Made smaller compressed cards

---
+++ frontend/src/components/blog/CompressedBlogCard.tsx
@@ -0,0 +1,29 @@
+const BlogCard: React.FC<{ post: any; onClick: () => void }> = ({
+  post,
+  onClick,
+}) => {
+  return (
+    <div
+      className="max-w-sm rounded-lg overflow-hidden shadow-lg border cursor-pointer"
+      onClick={onClick}
+    >
+      {/* Header */}
+      <h2 className="text-xl font-semibold p-4">{post.header}</h2>
+
+      {/* Image */}
+      {post.image1 && post.image1.asset && (
+        <div>
+          <img
+            className="w-full h-48 object-cover"
+            src={post.image1.asset.url}
+            alt={post.imageText1 || "Image 1"}
+          />
+        </div>
+      )}
+
+      {/* First text */}
+      {post.text1 && <p className="p-4">{post.text1}</p>}
+    </div>
+  );
+};
+export default BlogCard;
