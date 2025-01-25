f75a0d64bdf9ebfe8f3e82fb47f3924aab87e606
August Solli Middelkoop <august.kampen@gmail.com>
2025-01-25 15:19:55 +0100

    Enhancement: Updated artdisplay component to be facilities. Name change is not handled. Will fix.

--- sanity/schemaTypes/artDisplay.ts
+++ sanity/schemaTypes/artDisplay.ts
@@ -1,24 +1,57 @@
-import {defineField, defineType} from "sanity";
+import {defineField, defineType} from 'sanity'
 
 export default defineType({
-    name: "artDisplay",
-    title: "Art Display",
-    type: "document",
-    fields: [
-        defineField({
-            name: "image",
-            title: "Image",
-            type: "image",
-            options: {
-                hotspot: true,
-            },
-            description:"Provide the main image for the Page."
-        }),
-        defineField({
-            name: "alt",
-            title: "Alt Text",
-            type: "string",
-            description: "Provide a short description of the image for accessibility.",
-        }),
-    ],
-});
+  name: 'facilities',
+  title: 'Facilities',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'header',
+      title: 'Header',
+      type: 'string',
+      description: 'Here you can write the title of the blog post.',
+    }),
+    defineField({
+      name: 'text',
+      title: 'Text',
+      type: 'text',
+      description: 'Here you can write the first textual content of the facilities page.',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      description: 'Provide the main image for the Page.',
+    }),
+    defineField({
+      name: 'alt',
+      title: 'Alt Text',
+      type: 'string',
+      description: 'Provide a short description of the image for accessibility.',
+    }),
+    defineField({
+      name: 'text2',
+      title: 'Text 2',
+      type: 'text',
+      description: 'Here you can write the first textual content of the facilities page.',
+    }),
+    defineField({
+      name: 'image2',
+      title: 'Image 2',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      description: 'Provide the main image for the Page.',
+    }),
+    defineField({
+      name: 'alt2',
+      title: 'Alt Text',
+      type: 'string',
+      description: 'Provide a short description of the image for accessibility.',
+    }),
+  ],
+})
