8e410ad5161167ed02f36a436ed1b53a35e9532f
christgh <christopher.hoee@gmail.com>
2025-02-09 19:16:02 +0100

    fix: optimized code

--- frontend/src/pages/ApartmentPage.tsx
+++ frontend/src/pages/ApartmentPage.tsx
@@ -16,116 +16,112 @@ const ApartmentPage: React.FC = () => {
       header && item.header.toLowerCase().trim().replace(/ /g, "-") === header
   );
 
+  const additionalImages = facility?.carouselImages.slice(1);
+
   return (
-    <>
-      {!isLoading && facility && (
-        <>
-          <section className="flex flex-col gap-16 px-5 sm:px-10 pt-14">
-            <section className="flex flex-col md:flex-row gap-16">
-              <div className="flex flex-col space-y-4 md:w-1/2">
-                <h2 className="text-3xl font-bold">{facility.header}</h2>
-                {facility.textBlocks.map((text, index) => (
-                  <p key={index} className="text-lg">
-                    {text}
-                  </p>
-                ))}
-              </div>
-
-              <div className="flex flex-col md:w-1/2">
-                <img
-                  src={facility.carouselImages[0].asset.url}
-                  alt={facility.carouselImages[0].altText || "Additional image"}
-                  className="w-full h-full object-cover"
-                />
-                <p className="text-center">
-                  {facility.carouselImages[0].altText || ""}
+    !isLoading &&
+    facility &&
+    additionalImages && (
+      <>
+        <section className="flex flex-col gap-16 px-5 sm:px-10 pt-14">
+          <section className="flex flex-col md:flex-row gap-16">
+            <div className="flex flex-col space-y-4 md:w-1/2">
+              <h2 className="text-3xl font-bold">{facility.header}</h2>
+              {facility.textBlocks.map((text, index) => (
+                <p key={index} className="text-lg">
+                  {text}
                 </p>
-              </div>
-            </section>
+              ))}
+            </div>
 
-            {facility.carouselImages.slice(1).length < 4 && (
-              <>
-                {/* Mobile Layout */}
-                <section className="w-screen relative left-1/2 -translate-x-1/2 bg-[#1D192C] block sm:hidden">
-                  <div className="pt-[138px] pb-[64px] px-5">
-                    {/* First image on top */}
+            <div className="flex flex-col md:w-1/2">
+              <img
+                src={facility.carouselImages[0].asset.url}
+                alt={facility.carouselImages[0].altText || "Additional image"}
+                className="w-full h-full object-cover"
+              />
+              <p className="text-center">
+                {facility.carouselImages[0].altText || ""}
+              </p>
+            </div>
+          </section>
+
+          {additionalImages.length < 4 ? (
+            <>
+              {/* Mobile Layout */}
+              <section className="w-screen relative left-1/2 -translate-x-1/2 bg-[#1D192C] block sm:hidden">
+                <div className="pt-[138px] pb-[64px] px-5">
+                  {additionalImages[0] && (
                     <div className="mb-6">
                       <div className="relative w-full aspect-square overflow-hidden">
                         <img
-                          src={facility.carouselImages.slice(1)[0].asset.url}
+                          src={additionalImages[0].asset.url}
                           alt={
-                            facility.carouselImages.slice(1)[0].altText ||
-                            "Additional image"
+                            additionalImages[0].altText || "Additional image"
                           }
                           className="absolute inset-0 w-full h-full object-cover"
                         />
                       </div>
                       <p className="text-center text-white">
-                        {facility.carouselImages.slice(1)[0].altText ||
+                        {additionalImages[0].altText ||
                           "No description available"}
                       </p>
                     </div>
+                  )}
 
-                    {/* Remaining images in a row */}
-                    {facility.carouselImages.slice(1).length > 1 && (
-                      <div className="flex flex-row justify-center gap-6">
-                        {facility.carouselImages
-                          .slice(1)
-                          .slice(1)
-                          .map((image, index) => (
-                            <div
-                              key={index}
-                              className="flex flex-col gap-y-2 w-[150px]"
-                            >
-                              <div className="relative w-full aspect-square overflow-hidden">
-                                <img
-                                  src={image.asset.url}
-                                  alt={image.altText || "Additional image"}
-                                  className="absolute inset-0 w-full h-full object-cover"
-                                />
-                              </div>
-                              <p className="text-center text-white">
-                                {image.altText || "No description available"}
-                              </p>
-                            </div>
-                          ))}
-                      </div>
-                    )}
-                  </div>
-                </section>
-
-                {/* Larger Screens Layout */}
-                <section className="w-screen relative left-1/2 -translate-x-1/2 bg-[#1D192C] hidden sm:block">
-                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 pt-[138px] pb-[64px] lg:py-16 px-5 md:px-36 xl:px-64">
-                    {facility.carouselImages.slice(1).map((image, index) => (
-                      <div key={index} className="flex flex-col gap-y-2">
-                        <div className="relative w-full aspect-square overflow-hidden">
-                          <img
-                            src={image.asset.url}
-                            alt={image.altText || "Additional image"}
-                            className="absolute inset-0 w-full h-full object-cover"
-                          />
+                  {additionalImages.length > 1 && (
+                    <div className="flex flex-row justify-center gap-6">
+                      {additionalImages.slice(1).map((image, index) => (
+                        <div
+                          key={index}
+                          className="flex flex-col gap-y-2 w-[150px]"
+                        >
+                          <div className="relative w-full aspect-square overflow-hidden">
+                            <img
+                              src={image.asset.url}
+                              alt={image.altText || "Additional image"}
+                              className="absolute inset-0 w-full h-full object-cover"
+                            />
+                          </div>
+                          <p className="text-center text-white">
+                            {image.altText || "No description available"}
+                          </p>
                         </div>
-                        <p className="text-center text-white">
-                          {image.altText || "No description available"}
-                        </p>
-                      </div>
-                    ))}
-                  </div>
-                </section>
-              </>
-            )}
-
-            {facility.carouselImages.slice(1).length >= 4 && (
-              <section className="w-screen relative left-1/2 -translate-x-1/2 bg-[#1D192C] py-10">
-                <MyCarousel images={facility.carouselImages.slice(1)} />
+                      ))}
+                    </div>
+                  )}
+                </div>
               </section>
-            )}
-          </section>
-        </>
-      )}
-      <Footer />
-    </>
+
+              {/* Larger Screens Layout */}
+              <section className="w-screen relative left-1/2 -translate-x-1/2 bg-[#1D192C] hidden sm:block">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 pt-[138px] pb-[64px] lg:py-16 px-5 md:px-36 xl:px-64">
+                  {additionalImages.map((image, index) => (
+                    <div key={index} className="flex flex-col gap-y-2">
+                      <div className="relative w-full aspect-square overflow-hidden">
+                        <img
+                          src={image.asset.url}
+                          alt={image.altText || "Additional image"}
+                          className="absolute inset-0 w-full h-full object-cover"
+                        />
+                      </div>
+                      <p className="text-center text-white">
+                        {image.altText || "No description available"}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </section>
+            </>
+          ) : (
+            <section className="w-screen relative left-1/2 -translate-x-1/2 bg-[#1D192C] py-10">
+              <MyCarousel images={additionalImages} />
+            </section>
+          )}
+        </section>
+        <Footer />
+      </>
+    )
   );
 };
 
